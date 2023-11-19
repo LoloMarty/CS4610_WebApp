a05abdc0150cd6733cc36e7090ba7b2d0396820a
Carlos Castrillo <carcast1900@gmail.com>
2023-11-18 22:11:09 -0800

    Moved message components to a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-//import logo from "./logo.svg";
+import MessageInput from "./message";
 import "./App.css";
 
-function addText(){
-  alert('This is a test');
-}
 
 function App() {
   return (
     <>
       <h1>Welcome to Rettiwt!</h1>
       <h3>Project by Marvin, Nathan, Jonathan, and Carlos</h3>
-      <label> Message input: <input name="userMessage"></input></label>
-      <button onClick={addText}>Post it!</button>
+      <MessageInput />
     </>
   );
 }
