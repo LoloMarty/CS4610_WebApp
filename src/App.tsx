fdf66c1efaba8c8c38caeb72a3fd62afa364257b
Carlos Castrillo <carcast1900@gmail.com>
2023-11-18 20:07:42 -0800

    Added a text box and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
+
+function addText(){
+  alert('This is a test');
+}
 
 function App() {
   return (
     <>
-      <h1>Welcome to Rettiwt</h1>
+      <h1>Welcome to Rettiwt!</h1>
       <h3>Project by Marvin, Nathan, Jonathan, and Carlos</h3>
+      <label> Message input: <input name="userMessage"></input></label>
+      <button onClick={addText}>Post it!</button>
     </>
   );
 }
