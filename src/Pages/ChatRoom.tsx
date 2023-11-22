16e44f31d0ab46366409fb1d0c45a0d0359f9ea3
Carlos Castrillo <carcast1900@gmail.com>
2023-11-22 00:41:27 -0800

    Added backend functionality

--- src/Pages/ChatRoom.tsx
+++ src/Pages/ChatRoom.tsx
@@ -1,10 +1,19 @@
-import MessageInput from "../message";
+import React from "react";
 
 const ChatRoom = () => {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/messages")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <>
       <h1>Hi, this is the ChatRoom page</h1>
-      <MessageInput />
+      <p>{!data ? "Loading..." : data}</p>
     </>
   );
 };
