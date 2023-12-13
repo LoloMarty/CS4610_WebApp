29e7f0b65cdd0e1f0554ce5591134b70c4698169
nat-160 <natc160@gmail.com>
2023-12-13 15:50:31 -0800

    aslkjdfjaslkf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,29 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Login from "./routes/Login"
 import Update from "./routes/Update"
+import User from "./routes/User"
+import Register from "./routes/Register"
+import Home from "./routes/Home"
+import New from "./routes/New"
 import { UsersContextProvider } from "./context/UsersContext";
 
-const App = () =>
-{
+const App = () => {
 	return (
 		<UsersContextProvider>
 			<div>
 				<Router>
 					<Routes>
-						<Route exact path="/" Component={Login}/>
-						<Route exact path="/updateUser/:id/update" Component={Update}/>
+						<Route exact path="/user/:username" Component={User} />
+						<Route exact path="/test/login" Component={Login} />
+						<Route exact path="/test/register" Component={} />
+						<Route exact path="/test/home" Component={} />
+						<Route exact path="/test/new" Component={} />
 					</Routes>
 				</Router>
 			</div>
-		</UsersContextProvider>	
-)}
+		</UsersContextProvider>
+	)
+}
 
 export default App;
---
+++ client/src/routes/User.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import api from '../api/api'
+import Header from "../components/Header";
+
+export default function User() {
+    const { username } = useParams();
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await api.get(`/user/${username}`);
+            setData(response.data)
+        };
+        fetchData();
+    }, []);
+
+    function format() {
+        var arr = [];
+        for (const [key, value] of Object.entries(data)) {
+            arr.push(value)
+        }
+        return arr
+    }
+
+    return <div>
+        <Header />
+        {format().map((value) =>
+            <div>
+                <h3>{value.username}</h3>
+                <p>{value.message}</p>
+                <sub>At {value.timestamp}</sub>
+            </div>)}
+    </div>
+}
